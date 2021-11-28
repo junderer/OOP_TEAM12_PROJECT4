ea78cf398fd17b54a6686f8cf93c4d54ce107975
junderer <dlawnsgh0119@gmail.com>
2021-11-29 00:39:30 +0900

    git ignore opend db added and model cpp first commit

--- src/Model.cpp
+++ src/Model.cpp
@@ -1,2 +1,16 @@
 #include "Model.hpp"
 
+
+ /* ## Model public 메소드
+* void update()
+* Model의 state를 갱신 해주는 메소드
+* DeskComp* elements()
+* DeskComp 배열 반환용 메소드
+* void command(int)
+* ~번째 DeskComp의 고유 command 실행*/
+
+public void update() {
+
+};
+public DeskComp* elements();
+public void command(int cmdNum);
