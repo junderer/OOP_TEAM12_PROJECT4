ee888f5dbe063d245af1679560799b8c97b99063
junderer <dlawnsgh0119@gmail.com>
2021-11-29 18:10:44 +0900

    git ignore modified

--- src/Model.cpp
+++ src/Model.cpp
@@ -4,7 +4,7 @@
  /* ## Model public 메소드
 * void update()
 * Model의 state를 갱신 해주는 메소드
-* DeskComp* elements()
+* DeskComp* elements()   
 * DeskComp 배열 반환용 메소드
 * void command(int)
 * ~번째 DeskComp의 고유 command 실행*/
